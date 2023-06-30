bc64cd7f6e8eac0bbef0933787ac180a1426aabd
ZayanaKhan <97752560+ZayanaKhan@users.noreply.github.com>
2023-06-30 00:01:54 -0400

    Added Device Page

---
+++ src/page-components/APIDevicePage.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+    import { deviceAll } from "../lib/datamodel.js";
+    import DeviceAll from "../api-components/DeviceAll.svelte";
+
+    let deviceList;
+    $: deviceList = $deviceAll;
+</script>
+
+<h2>Devices</h2>
+
+<DeviceAll refreshTimer={10000} />
+
+<!-- Display the messages -->
+{#each deviceList as device, i}
+    <ul>
+        Identifier: {device.identifier} <br />
+        Panel: {device.panel}
+    </ul>
+    <hr />
+{/each}
